48eff7c7ad7550f8522c6c233e2b51be3d646765
FranquiPI <pizarrofranco25@gmail.com>
2024-08-22 18:06:22 -0300

    Clase 1 de JavaScript 4to Semestre
    Clase 01 E-Commerce Básico parte 01

---
+++ Javascript/e-commerce/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/e-commerce/client/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/e-commerce/client/media/leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 3500,
+        quanty: 1,
+        img: "/e-commerce/client/media/pollo.png",
+    },
+    {
+        id: 1,
+        productName: "Mayonesa",
+        price: 1200,
+        quanty: 1,
+        img: "/e-commerce/client/media/mayonesa.png",
+    }
+]
