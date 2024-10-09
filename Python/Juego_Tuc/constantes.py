36a054558e1ae6176770ac18099d8fbbd7eac2e2
FranquiPI <pizarrofranco25@gmail.com>
2024-10-09 12:14:42 -0300

    Clase 8 Proyecto nuevo parte 2
    Creamos nueva carpeta Juegos_tuc para un nuevo proyecto sobre pygame

---
+++ Python/Juego_Tuc/constantes.py
@@ -0,0 +1,14 @@
+# Constantes.py
+
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 1700
+SCREEN_HEIGTH = 1000
+
+# Ruta de los assets
+ASSETS_PATH = "C:/Users/Franco/TUP/4to Semestre/Python/Juego_Tuc/assets"
+
+#Ruta de los archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sounds/ IMPERIALMARCH - Kenobi.mp3'
+START_IMAGE_PATH = f'{ASSETS_PATH}/image/inicio/star.png'
+ESTRELLA_PATH = f'{ASSETS_PATH}/image/inicio/estrella'
+FONDO1_PATH = f'{ASSETS_PATH}/image/fondo1.jpg'
