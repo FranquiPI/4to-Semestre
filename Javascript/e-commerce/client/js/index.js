fe49ee247b4a457a54b9c3a2440923765b2c9f26
FranquiPI <pizarrofranco25@gmail.com>
2024-10-10 16:12:58 -0300

    Clase 03 }E-commerce Básico parte03
    Avanzamos en el Body, Footer,  Configuramos las cantidades de productos, Botones de suma y resta de productos, Calcular el total de la compra, Eliminar productos del modal -> Parte 1 y 2

--- Javascript/e-commerce/client/js/index.js
+++ Javascript/e-commerce/client/js/index.js
@@ -3,10 +3,11 @@ const cart = []; //Este es nuestro carrito, un array vacío.
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
-    <img src="${product.img}"></img>
+    <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+    <p class="price">${product.price}$</p>
     `;
     shopContent.append(content);
 
@@ -16,14 +17,22 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
 
-    })
+        if(repeat){
+            cart.map((prod)=>{
+                if(prod.if === product.id){
+                    prod.quanty++;
+                }
+            })
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
+    });
 });
